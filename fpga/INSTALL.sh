f71eaaad1c90ec3e01bda9038f49838cc9e726b3
tim <tim@elec.ac.nz>
2022-09-20 10:12:14 +1200

    Added module signature

---
+++ fpga/INSTALL.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Install the tools required to build the FPGA
+#
+pip3 install yowasp-yosys --upgrade
+pip3 install yowasp-nextpnr-gowin --upgrade
--- fpga/radio_module.v
+++ fpga/radio_module.v
@@ -1,14 +1,18 @@
 module top (
-    input SYS_CLK,
-    output reg LED_R
+    input DATA_CLK,
+    input [1:0] R1_I,
+    input [1:0] R1_Q,
+    input [1:0] R2_I,
+    input [1:0] R2_Q,
+    output reg DATA_OUT
 );
     // Set up a counter
     reg [25:0] ctr_q = 0;
 
-    always @(posedge SYS_CLK)
+    always @(posedge DATA_CLK)
     begin
         ctr_q <= ctr_q + 1;
-        LED_R <= ctr_q[23];
+        DATA_OUT <= ctr_q[23];
     end
 
 endmodule
