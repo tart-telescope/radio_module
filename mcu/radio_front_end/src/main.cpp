301f6830de973e35694ab5b22697f89ce02f7665
Tim Molteno <tim@4i.nz>
2023-03-06 09:02:20 +1300

    Some slight updates

--- mcu/radio_front_end/src/main.cpp
+++ mcu/radio_front_end/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <SPI.h>
 
 // Board definitions
 
@@ -16,6 +17,7 @@
 #define IN_ANT_B 19
 
 
+// Enable component power
 #define EN_A 34
 #define EN_B 35
 #define EN_FPGA 36
@@ -23,12 +25,13 @@
 
 // Serial Connection to Radio A, B
 #define DOUT 48
-#define DIN 1
+#define DIN 1 
 #define DCLK 2
 #define CS_A_NEG 23
 #define CS_B_NEG 24
 
 // Button on Board
+
 
 #define SW_1 47
 
@@ -40,20 +43,55 @@
 
 
 
+void writeRadioA() {
+    // Programme The Radios
+  // set up 3-wire interface for radio A
+
+  digitalWrite(CS_A_NEG, 0);    // select radio A
+  digitalWrite(CS_B_NEG, 1);    // select radio A
+  SPISettings radio_a(14000000, MSBFIRST, SPI_MODE0);
+
+  SPI.beginTransaction(radio_a);
+}
+
+void setupRadios() {
+  pinMode(EN_A, OUTPUT);
+  pinMode(EN_B, OUTPUT);
+
+  pinMode(CS_A_NEG, OUTPUT);
+  pinMode(CS_B_NEG, OUTPUT);
+  pinMode(DOUT, OUTPUT);
+  pinMode(DCLK, OUTPUT);
+  
+  // power on radios
+  digitalWrite(EN_A, 1);
+  digitalWrite(EN_B, 1);
+
+
+  sleep(100); // wait for a little before programming. PGM is held low
+
+  digitalWrite(DOUT, 0);
+  digitalWrite(DCLK, 0);
+  digitalWrite(CS_A_NEG, 0);
+  digitalWrite(CS_B_NEG, 0);
+}
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_LD_A, OUTPUT);
   pinMode(LED_LD_B, OUTPUT);
   pinMode(LED_ANT_A, OUTPUT);
   pinMode(LED_ANT_B, OUTPUT);
+
+  setupRadios();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED_LD_A, HIGH);
-  digitalWrite(LED_LD_B, LOW);
-  delay(3000);
-  digitalWrite(LED_LD_A, LOW);
-  digitalWrite(LED_LD_B, HIGH);
-  delay(1000);
+
+  int ld_a = digitalRead(IN_LD_A);
+  int ld_b = digitalRead(IN_LD_B);
+
+  digitalWrite(LED_LD_A, ld_a);
+  digitalWrite(LED_LD_B, ld_b);
 }
