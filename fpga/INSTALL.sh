c394992accddb0372ccc5bb671088e8b8a4366da
tim <tim@elec.ac.nz>
2023-03-17 15:20:42 +1300

    Use new install scripts

--- fpga/INSTALL.sh
+++ fpga/INSTALL.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Install the tools required to build the FPGA
 #
-pip3 install yowasp-yosys --upgrade
-pip3 install yowasp-nextpnr-gowin --upgrade
+pip3 install yowasp-yosys --upgrade --break-system-packages
+pip3 install yowasp-nextpnr-gowin --upgrade --break-system-packages
