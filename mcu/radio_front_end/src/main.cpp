6baefb8a890e8387306335563f2cde1d04c937bd
tim <tim@elec.ac.nz>
2023-03-21 11:33:47 +1300

    Add some MAX2769 SPI writing

--- mcu/radio_front_end/src/main.cpp
+++ mcu/radio_front_end/src/main.cpp
@@ -45,21 +45,38 @@
 
 // SPI mapping
 
+// Max2769 register addresses
+#define MAX_2769_CONF1 0
+#define MAX_2769_CONF2 1
+#define MAX_2769_CONF3 2
+#define MAX_2769_PLLCONF 3
+#define MAX_2769_DIV 4
+#define MAX_2769_FDIV 5
+#define MAX_2769_STRM 6
+#define MAX_2769_CLK 7
+
+void max2769set(uint8_t cs_pin, 
+                uint8_t addr, 
+                uint32_t data) {
+  digitalWrite(cs_pin, 0);
+
+  // form the 32-bit word to send, consisting of the address as the lower 4 bits
+  uint32_t word = (addr & 0x0F) || (data << 4);
+  SPI.transfer((word >> 24) & 0x0000FF);
+  SPI.transfer(word >> 16  & 0x0000FF);
+  SPI.transfer(word >> 8 & 0x0000FF);
+  SPI.transfer(word & 0x0000FF);
+  digitalWrite(cs_pin, 1);
+}
 
 void setupRadio(uint8_t cs_pin) {
+  pinMode(PIN_SPI_MISO, INPUT);
+  pinMode(PIN_SPI_MOSI, OUTPUT);
+  pinMode(PIN_SPI_SCK, OUTPUT);
   SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
-  digitalWrite(cs_pin, 0);
-  SPI.transfer(0X55);
-  SPI.transfer(0X55);
-  SPI.transfer(0X55);
-  SPI.transfer(0X55);
-  SPI.transfer(0X55);
-  SPI.transfer(0X55);
-  SPI.transfer(0X55);
-  SPI.transfer(0X55);
-  SPI.transfer(0X55);
-  SPI.transfer(0X55);
-  digitalWrite(cs_pin, 1);
+
+  max2769set(cs_pin, MAX_2769_CONF1, 0xA2919A3);
+  SPI.endTransaction();
 }
 
 void setup() {
@@ -75,18 +92,22 @@ void setup() {
   pinMode(CS_A_NEG, PIN_DIR_OUTPUT);
   pinMode(CS_B_NEG, PIN_DIR_OUTPUT);
 
+  // Power on the FPGA - this gets the clock going for the radios.
+  pinMode(EN_FPGA_NEG, OUTPUT);
+  digitalWrite(EN_FPGA_NEG, LOW);
+  delay(100); // wait 100 ms
+
+  // Power on the radios
   pinMode(EN_A_NEG, OUTPUT);
   pinMode(EN_B_NEG, OUTPUT);
-  pinMode(EN_FPGA_NEG, OUTPUT);
 
   digitalWrite(EN_A_NEG, LOW);
   digitalWrite(EN_B_NEG, LOW);
-  digitalWrite(EN_FPGA_NEG, LOW);
   
-  delay(1000);
+  delay(100);
 
- // setupRadio(CS_A_NEG);
- // setupRadio(CS_B_NEG);
+  // setupRadio(CS_A_NEG);
+  // setupRadio(CS_B_NEG);
 }
 
 void loop() {
