b2c733900da97c3751054b53eebd75b993b5cd45
tim <tim@elec.ac.nz>
2023-05-03 13:45:37 +1200

    Now have config mostly going. IQ still to do

--- hardware/dev_board/tart_radio_config/doc/max2769_conf.py
+++ hardware/dev_board/tart_radio_config/doc/max2769_conf.py
@@ -23,23 +23,27 @@ class Register:
         return ret
     
     def disp(self):
-        print(f"  max2769set(cs_pin, 0b{self.address}, 0x{self.getValue() :x});  // {self.name}")
+        print(f"  max2769set(cs_port, cs_pin, 0b{self.address}, 0x{self.getValue() :x});  // {self.name}")
 
 def getConf1():
     reg = Register("CONF1", "0000")
     reg.setBit(27, "1");   # ret[27] = 1; enable
-    reg.setBit(26, "0");   # IDLE off
+    reg.setBit(26, "0");   # IDLE Idle enable. Set 1 to put the chip in the idle mode and 0 for operating mode.
     reg.setBits(25, 22, "1000");   # Reserved
     reg.setBits(21, 20, "10");   # Reserved
     reg.setBits(19, 18, "10");   # Reserved
     reg.setBits(17, 16, "01");   # Reserved
     reg.setBit(15, "0");   # MIXPOLE
-    reg.setBits(14, 13, "01");   # ret[14:13] = 01 LNA2 on
+    reg.setBits(14, 13, "01");   # ret[14:13] = '01': LNA2 on, '00': Choose with ant bias
     reg.setBit(12, "1");   # MIXEN enable mixer
-    reg.setBit(11, "1");   # ANTEN enable antenna
-    reg.setBits(10, 5, "001011"); # FCEN = 4.092 MHz
-    reg.setBits(4, 3, "10"); # RBW = 4.2 MHz
-    reg.setBit(2, "0"); # 5th order Butterworth
+    reg.setBit(11, "1");   # ANTEN enable antenna bias
+    reg.setBits(10, 5, "001011"); # FCEN 
+                                  #  IF center frequency programming.
+                                  #     '001101' = 3.092MHz, 
+                                  #     '001011' = 4.092MHz
+                                  #     '010011' = 10.0MHz
+    reg.setBits(4, 3, "10");      # RBW = '00': 2.5MHz, '10': 4.2 MHz, '01': 9.66MHz
+    reg.setBit(2, "1"); # 0: 3rd order, 1: 5th order Butterworth
     reg.setBit(1, "1"); # Complex bandpass filter mode
     reg.setBit(0, "1"); # Keep filter gain
     return reg
@@ -47,15 +51,15 @@ def getConf1():
 
 def getConf2():
     reg = Register("CONF2", "0001")
-    reg.setBit(27, "0");   # IQEN enable (I channel only is 0)
+    reg.setBit(27, "1");   # IQEN enable (I channel only is 0)
     reg.setBits(26, 15, "000010101010");   # GAINREF
     reg.setBits(14, 13, "00");   # Reserved
     reg.setBits(12, 11, "00");   # AGC Mode INDEPENDENT I/Q
-    reg.setBits(10, 9, "10");   # FORMAT Sign Magnitude
-    reg.setBits(8, 6, "000");   # 1 bits ADC (010 is 2-bits)
-    reg.setBits(5, 4, "00");   # DRVCFG CMOS logic
+    reg.setBits(10, 9, "01");   # FORMAT '01': Sign Magnitude, 
+    reg.setBits(8, 6, "100");   # 1 bits ADC ('000': 1-bit, '010': 2-bits, '100': 3-bits)0
+    reg.setBits(5, 4, "00");   # DRVCFG '00': CMOS logic, '01': Differential?, '1x': Analog
     reg.setBit(3, "1");   # Reserved
-    reg.setBit(2, "0");   # Reserved
+    reg.setBit(2, "1");   # Reserved
     reg.setBits(1, 0, "00");   # DIEID
     return reg
 
@@ -63,7 +67,7 @@ def getConf3Normal():
     reg = Register("CONF3", "0010")
     reg.setBits(27, 22, "111010");   # GAININ 57 dB
     reg.setBit(21, "1");   # RESERVED
-    reg.setBit(20, "0");   # HIGHLOADEN
+    reg.setBit(20, "0");   # HIGHLOADEN # TODO
     reg.setBit(19, "1");   # RESERVED
     reg.setBit(18, "1");   # RESERVED
     reg.setBit(17, "1");   # RESERVED
@@ -84,8 +88,8 @@ def getConf3Normal():
     return reg
 
 
-def getConf3Stream():
-    reg = Register("CONF3", "0010")
+def getConf3Stream(start=0):
+    reg = Register(f"CONF3 (Streaming) start={start}", "0010")
     reg.setBits(27, 22, "111010");   # GAININ 57 dB
     reg.setBit(21, "1");   # RESERVED
     reg.setBit(20, "0");   # HIGHLOADEN
@@ -98,16 +102,16 @@ def getConf3Stream():
     reg.setBit(13, "1");   # RESERVED
     reg.setBit(12, "0");   # RESERVED
     reg.setBit(11, "1");   # STRMEN
-    reg.setBit(10, "1");   # STRMSTART
+    reg.setBit(10, f"{start}");   # STRMSTART
     reg.setBit(9, "0");   # STRMSTOP
     reg.setBits(8, 6, "111");   # RESERVED
-    reg.setBits(5, 4, "11");   # NUMBER of bits streamed STRMBITS 
+    reg.setBits(5, 4, "10");   # NUMBER of bits streamed STRMBITS 
                             #       00: I MSB
                             #       01: I MSB, I LSB,
                             #       10: I MSB, Q MSB,
                             #       11: I MSB, I LSB, Q MSB, Q LSB,
     reg.setBit(3, "1");   # STAMPEN
-    reg.setBit(2, "1");   # TIMESYNCEN
+    reg.setBit(2, "0");   # TIMESYNCEN
     reg.setBit(1, "1");   # DATASYNCEN
     reg.setBit(0, "0");   # STRMRST Reset all counters
     return reg
@@ -133,8 +137,23 @@ def getPLL():
     reg.setBit(1, 0);   # RESERVED
     return reg
 
+import argparse
+
 if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='Configure Max2769 Registers radio telescope.',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--stream', '-s', action="store_true",
+                        help="Enable streaming")
+    ARGS = parser.parse_args()
+
     getConf1().disp()
     getConf2().disp()
     getConf3Normal().disp()
+    if ARGS.stream:
+        getConf3Stream(start=0).disp()
     getPLL().disp()
+
+    if ARGS.stream:
+        getConf3Stream(start=1).disp()
+
--- hardware/dev_board/tart_radio_config/src/max2769.c
+++ hardware/dev_board/tart_radio_config/src/max2769.c
@@ -2,7 +2,6 @@
 #include <debug.h>
 #include "hardware.h"
 
-
 // Max2769_handler
 
 // Max2769 register addresses
@@ -15,39 +14,50 @@
 #define MAX_2769_STRM 6
 #define MAX_2769_CLK 7
 
-void max2769set(GPIO_TypeDef* cs_port, pin_t cs_pin,
-                uint8_t addr, 
-                uint32_t data) {
+void max2769set(GPIO_TypeDef *cs_port, pin_t cs_pin,
+                uint8_t addr,
+                uint32_t data)
+{
 
-  int i;
-  uint32_t word = (addr & 0x0F) | (data << 4);
-  GPIO_WriteBit(cs_port, cs_pin, Bit_RESET);
-  GPIO_WriteBit(TART_SCLK_PORT, TART_SCLK_PIN, Bit_RESET);
-
-  Delay_Us(5);
-
-  for (i=31;i>=0;i--) {
-    uint8_t bit = (word >> i) & 0x00000001L;
-    	
-    if (0 == bit) 
-        GPIO_WriteBit(TART_SDATA_PORT, TART_SDATA_PIN, Bit_RESET);
-    else
-        GPIO_WriteBit(TART_SDATA_PORT, TART_SDATA_PIN, Bit_SET);
-    Delay_Us(10);
-    GPIO_WriteBit(TART_SCLK_PORT, TART_SCLK_PIN, Bit_SET);
-    Delay_Us(10);
+    int i;
+    uint32_t word = (addr & 0x0F) | (data << 4);
+    GPIO_WriteBit(cs_port, cs_pin, Bit_RESET);
     GPIO_WriteBit(TART_SCLK_PORT, TART_SCLK_PIN, Bit_RESET);
-    Delay_Us(10);
-  }
-  GPIO_WriteBit(cs_port, cs_pin, Bit_SET);
-  Delay_Ms(1);
+
+    int t_CSS = 10;
+    int t_DS = 10;
+    int t_DH = 10;
+    int t_CH = 25;
+    int t_CL = 25;
+    int t_CSH = 10;
+    int t_CSW = t_CH + t_CL;
+
+    Delay_Us(t_CSS); // t_CSS
+
+    for (i = 31; i >= 0; i--)
+    {
+        uint8_t bit = (word >> i) & 0x00000001L;
+
+        if (0 == bit)
+            GPIO_WriteBit(TART_SDATA_PORT, TART_SDATA_PIN, Bit_RESET);
+        else
+            GPIO_WriteBit(TART_SDATA_PORT, TART_SDATA_PIN, Bit_SET);
+
+        Delay_Us(t_DS); // t_DS = 10
+        GPIO_WriteBit(TART_SCLK_PORT, TART_SCLK_PIN, Bit_SET);
+        Delay_Us(t_CH); // t_CH = 25
+        GPIO_WriteBit(TART_SCLK_PORT, TART_SCLK_PIN, Bit_RESET);
+        Delay_Us(t_CL); // t_CL
+    }
+    GPIO_WriteBit(cs_port, cs_pin, Bit_SET);
+    Delay_Ms(1);
 }
 
-
-void setupRadio(GPIO_TypeDef* cs_port, pin_t cs_pin) {
-  // These are set up using the python script in the doc directory.
-  max2769set(cs_port, cs_pin, 0b0000, 0xa2919A3);  // CONF1
-  max2769set(cs_port, cs_pin, 0b0001, 0x055028C);  // CONF2
+void setupRadio(GPIO_TypeDef *cs_port, pin_t cs_pin)
+{
+    // These are set up using the python script in the doc directory.
+  max2769set(cs_port, cs_pin, 0b0000, 0xa293977);  // CONF1
+  max2769set(cs_port, cs_pin, 0b0001, 0x855030c);  // CONF2
   max2769set(cs_port, cs_pin, 0b0010, 0xeafe1dc);  // CONF3
   max2769set(cs_port, cs_pin, 0b0011, 0x9ec0008);  // PLL
-}
+  }
